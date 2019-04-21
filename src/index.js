c44826c6bf4309d69f58b062bbd3760d9559b7c9
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-21 18:34:32 -0500

    Demonstrate basics of click event handling, passing props between components.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,29 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
+  /* state should be considered as private to a React component */
+  constructor(props) {
+    /* in JS, everything that has a constructor should first make a call to the parent class via super. React components should make super call with super(props)*/
+    super(props);
+
+    this.state = {
+      value: null
+    };
+  }
+
+  /* re-render the square that corresponds with the updated value in this.state, 'X' or 'O', when the button is clicked */
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
